3079390a399a0f6408d55a4558d21e2395006e8a
EmmaNGagen <emma.n.gagen@outlook.com>
2021-12-28 12:53:11 +0100

    header och navigation

---
+++ src/ts/main.ts
@@ -0,0 +1,7 @@
+window.onload = function () {
+  theBarNav();
+};
+
+export function theBarNav() {}
+let hamburger: HTMLDivElement = document.querySelector("burgerBtn");
+let navMenu: HTMLUListElement = document.querySelector(".nav-menu");
