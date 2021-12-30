46720d7cc21af9c93c44e63881ea3e0fe594ee24
EmmaNGagen <emma.n.gagen@outlook.com>
2021-12-30 12:20:02 +0100

    css och js landingpage bloggar

--- src/ts/functions/displayBlogs.ts
+++ src/ts/functions/displayBlogs.ts
@@ -2,13 +2,43 @@ import { listOfObjects } from "../modules/bloggModule";
 export function displayBlogs() {
   console.log(listOfObjects);
   const mainWrapper: HTMLDivElement = document.getElementById(
-    "mainWrapper"
+    "flexRow"
   ) as HTMLDivElement;
   for (let i = 0; i < listOfObjects.length; i++) {
+    //container för conten
     const div: HTMLDivElement = document.createElement("div");
-    div.className = "imgContainer";
-    div.setAttribute("style", `background-image:url(${listOfObjects[i].img})`);
-    console.log(listOfObjects[i].img);
+    div.className = "contentContainer";
     mainWrapper.appendChild(div);
+    //id
+    const bloggId: HTMLSpanElement = document.createElement("span");
+    bloggId.className = "idOfBlogg";
+    bloggId.innerHTML = listOfObjects[i].id;
+    //titlen
+    const spanTitle: HTMLSpanElement = document.createElement("span");
+    spanTitle.className = "titleContent";
+    spanTitle.innerHTML = listOfObjects[i].title;
+    div.appendChild(spanTitle);
+
+    //bilden
+    const imgDiv: HTMLDivElement = document.createElement("div");
+    imgDiv.className = "imgContainer";
+    imgDiv.setAttribute(
+      "style",
+      `background-image:url(${listOfObjects[i].img})`
+    );
+    console.log(listOfObjects[i].img);
+    div.appendChild(imgDiv);
+
+    //User
+    const userTitle: HTMLSpanElement = document.createElement("p");
+    userTitle.className = "userTitle";
+    userTitle.innerHTML = listOfObjects[i].user;
+    div.appendChild(userTitle);
+
+    //Date
+    const userDate: HTMLSpanElement = document.createElement("p");
+    userDate.className = "userDate";
+    userDate.innerHTML = listOfObjects[i].date;
+    div.appendChild(userDate);
   }
 }
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,6 @@
 import { theBarNav } from "./header";
 import { displayBlogs } from "./functions/displayBlogs";
+
 window.onload = function () {
   theBarNav();
   displayBlogs();
