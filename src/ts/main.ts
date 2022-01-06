e177e5460b1789fa6c89ac128792d95cfef7b181
EmmaNGagen <emma.n.gagen@outlook.com>
2022-01-06 21:05:07 +0100

    function on post page

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,7 +1,9 @@
 import { theBarNav } from "./header";
 import { displayBlogs } from "./functions/displayBlogs";
+import { displayPost } from "./post";
 
 window.onload = function () {
   theBarNav();
   displayBlogs();
+  displayPost();
 };
